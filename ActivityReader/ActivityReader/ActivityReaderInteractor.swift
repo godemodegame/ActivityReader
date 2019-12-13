e19d6b153921a906b145b9c2abe630c082f51f2a
godemodegame <godemodegame@icloud.com>
2019-12-13 20:56:05 +0300

    add write to csv

--- ActivityReader/ActivityReader/ActivityReaderInteractor.swift
+++ ActivityReader/ActivityReader/ActivityReaderInteractor.swift
@@ -1,4 +1,5 @@
 import Foundation
+import CSV
 
 protocol ActivityReaderBusinessLogic {
     func makeRequest(request: ActivityReader.Model.Request.RequestType)
@@ -8,6 +9,8 @@ class ActivityReaderInteractor: ActivityReaderBusinessLogic {
     
     public var presenter: ActivityReaderPresentationLogic?
     private var service: ActivityReaderService?
+    
+    let stream = OutputStream(toFileAtPath: "", append: true)!
     
     var data: [ActivityReader.Acceleration] = []
     
@@ -30,11 +33,19 @@ class ActivityReaderInteractor: ActivityReaderBusinessLogic {
                 self.service?.stopAccelerometer()
             }
         case .save(let text):
+            do {
+                let csv = try CSVWriter(stream: self.stream)
+                try self.data.forEach {
+                    try csv.write(row: ["\($0.x)", "\($0.y)", "\($0.z)", text])
+                }
+                csv.stream.close()
+            } catch {
+                print("error to write csv")
+            }
             print("save \(text)\n\(self.data)")
             self.data = []
         case .delete:
             self.data = []
         }
     }
-    
 }
